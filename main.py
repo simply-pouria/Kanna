fa13060e8d12c9d1a0132c01b2b79cb4c94a9791
pouria :) <77992542+simply-pouria@users.noreply.github.com>
2021-08-21 09:07:44 +0000

    solved the issue about .

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ from keep_me import keep_alive
 from replit import db
 
 
+F1 = os.environ['F1']
+ 
+F2 = os.environ['F2']
 
-sad_words_response = ["ولش بابا خودتو اذیت نکن ", "این نیز بگذرد بمولا", "اصن ارزششو نداره", "بیخیال ولش باو ",
-                          "چیشده؟ برام بگو"]
+S1 = os.environ['S1']
 
-sad_words=["هعی","تف","ناراحتم","شت","شتت","هعی داق","ریدم","فاک","فاااک","اه"]
+S2 = os.environ['S2']
+
+R1 = os.environ['R1']
+
+token = os.environ['token']
+
+
+sad_words_response = ["ولش بابا خودتو اذیت نکن ", "این نیز بگذرد بمولا", "اصن ارزششو نداره",
+"بیخیال ولش باو ","چیشده؟ برام بگو"]
+
+sad_words=["هعی","تف","ناراحتم",S1,S2,"هعی داق",R1,F1,F2,"اه"]
 
 learned_input = []
 
@@ -28,12 +40,13 @@ for i in keys:
 def db_reset() :
   for x in keys:
     del db[x]
-
-
-#db_reset()
+    
+    
+db_reset()
 
 
 flag = False
+flag_2 = False
 _input = None
 _output = None
 
@@ -60,25 +73,36 @@ def main():
         await ms.channel.send("بححح سلاممم")
 
     elif msg == "$delete":
+      global flag_2
+      flag_2 = True
       await ms.channel.send("چه کلمه ای رو میخوای حذف کنی؟")
+      
       @cl.event
       async def on_message(ms4):
-        for x in learned_output:
-          if x == ms4.content:
-            ind=learned_output.index(ms4)
-            del db [ms4]
-            del db [learned_input[ind]]
-            del learned_input[ind]
-            learned_output.remove(ms4)
-            await ms.channel.send("حذف شد")
-          else:
-            await ms.channel.send("اصلا من همچین کلمه ای رو بلد نیستم")
+        global flag_2
+        if flag_2:
+          for x in learned_output:
+            if not ms4.author == cl.user and ms4.author == ms.author and not ms4.content == "$delete" :
+              if x == ms4.content:
+                out = ms4.content
+                inp = learned_input[learned_output.index(out)]
+                del db [out]
+                del db [inp]
+                learned_input.remove(inp)
+                learned_output.remove(out)
+                await ms.channel.send("حذف شد")
+                flag_2 = False
+                main()
+              else:
+                await ms.channel.send("اصلا من همچین کلمه ای رو بلد نیستم")
+                flag_2 = False
+                main()
 
 
 
     
     elif msg.startswith("راهنما") or msg.startswith("help"):
-        await ms.channel.send("$teach برای یاد دادن کلمه ها کافیه بنویسی")
+        await ms.channel.send("$delete برای پاک کردنشون بنویس  $teach برای یاد دادن کلمه ها کافیه بنویسی")
 
     elif msg.startswith("چطوری") or msg.startswith("خوبی"):
         await ms.channel.send(random.choice(["خوبم مرسی","خوبم ممنون","هعی میگذرونیم",
@@ -101,6 +125,7 @@ def main():
             global flag
             if flag:
                 msg = ms2.content
+                
                 if not msg == "$teach" and not ms2.author == cl.user and ms2.author == ms.author:
                     global _output
                     _output = msg
@@ -118,9 +143,10 @@ def main():
                                 learned_input.append(_input)
                                 learned_output.append(_output)
                                 await ms.channel.send("حله")
-                                main()
                                 db[_output] = "o1"
                                 db[_input] = "i1"
+                                main()
+                                
 
 
     global learned_input, learned_output
@@ -131,6 +157,5 @@ def main():
         await ms.channel.send(proper_output)
 
 main()
-token = os.environ['token']
 keep_alive()
 cl.run(token)
